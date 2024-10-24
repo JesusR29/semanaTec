d15b30e5af47071355476ec60d9d7135b10ab882
Yael Ramos <jesus29yael@gmail.com>
2024-10-24 03:01:48 -0600

    Added colors and randomized for each new game

--- snake.py
+++ snake.py
@@ -1,7 +1,5 @@
 """Snake, classic arcade game.
-
 Exercises
-
 1. How do you make the snake faster or slower?
 2. How can you make the snake go around the edges?
 3. How would you move the food?
@@ -16,6 +14,7 @@ from freegames import square, vector
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+colors = ['purple', 'green', 'blue', 'orange', 'gray' ]	
 
 
 def change(x, y):
@@ -52,12 +51,25 @@ def move():
 
     for body in snake:
         square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, snake_color)
 
     square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, food_color)
     update()
     ontimer(move, 100)
 
 
+def set_colors():
+    """Change colors of snake and food."""
+    global snake_color
+    global food_color
+    
+    snake_color = colors[randrange(len(colors))]
+    food_color = colors[randrange(len(colors))]
+    # Ensure food color is not the same as snake color
+    while food_color == snake_color:
+        food_color = colors[randrange(len(colors))]
+    return snake_color, food_color
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
@@ -66,5 +78,6 @@ onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+set_colors()
 move()
 done()
